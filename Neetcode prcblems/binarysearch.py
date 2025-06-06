65dad6c580be5c187df4f3277e1c89954cb3bd1a
MarColtmx <mavaldezc@coltomex.com>
2025-06-06 16:58:20 -0600

    Updating binary search problem

--- Neetcode prcblems/binarysearch.py
+++ Neetcode prcblems/binarysearch.py
@@ -1,4 +1,5 @@
 from typing import List
+import bisect
 
 """My solution"""
 
@@ -35,38 +36,60 @@ from typing import List
 #         return self.binary_search(0, len(nums) - 1, nums, target)
 
 """Iterative Binary Search Solution"""
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         l, r = 0, len(nums) - 1
 
-        while l <= r:
-            # (l + r) // 2 can lead to overflow
-            m = l + ((r - l) // 2)  
+#         while l <= r:
+#             # (l + r) // 2 can lead to overflow
+#             m = l + ((r - l) // 2)  
 
-            if nums[m] > target:
-                r = m - 1
-            elif nums[m] < target:
-                l = m + 1
-            else:
-                return m
-        return -1
+#             if nums[m] > target:
+#                 r = m - 1
+#             elif nums[m] < target:
+#                 l = m + 1
+#             else:
+#                 return m
+#         return -1
 
 nums = [-1,0,2,4,6,8]
 target = 4
 
 """Upper Bund Solution"""
 
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         l, r = 0, len(nums)
+
+#         while l < r:
+#             m = l + ((r - l) // 2)  
+#             if nums[m] > target:
+#                 r = m
+#             elif nums[m] <= target:
+#                 l = m + 1
+#         return l - 1 if (l and nums[l - 1] == target) else -1
+
+"""Lower Bunnd Solution"""
+
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         l, r = 0, len(nums)
+
+#         while l < r:
+#             m = l + ((r - l) // 2)  
+#             if nums[m] >= target:
+#                 r = m
+#             elif nums[m] < target:
+#                 l = m + 1
+#         return l if (l < len(nums) and nums[l] == target) else -1
+    
+"""Built-In Solution"""
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums)
-
-        while l < r:
-            m = l + ((r - l) // 2)  
-            if nums[m] > target:
-                r = m
-            elif nums[m] <= target:
-                l = m + 1
-        return l - 1 if (l and nums[l - 1] == target) else -1
+        index = bisect.bisect_left(nums, target)
+        return index if index < len(nums) and nums[index] == target else -1
 
 s = Solution()
 print(s.search(nums,target))
